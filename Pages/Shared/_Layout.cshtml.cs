06a3bb69e284faca874714d10ade07289da2859f
TacoConKvass <e_frun@o2.pl>
2023-11-25 21:32:21 +0100

    [ Fix ] - API works now! [ Change ] - Submit quote css

--- Pages/Shared/_Layout.cshtml.cs
+++ Pages/Shared/_Layout.cshtml.cs
@@ -5,9 +5,5 @@ namespace ITClassProject.Pages.Shared
 {
 	public class _LayoutModel : _PageModel
 	{
-		public void OnPost() {
-			var bonk = Request.Form["sign-out"];
-			Console.WriteLine(bonk);
-		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using MySqlConnector;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
+builder.Services.AddControllers();
 builder.Services.AddTransient<MySqlConnection>(_ =>
     new MySqlConnection(Config.ConnectionString));
 
@@ -17,6 +18,7 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseSession();
 
+app.MapControllers();
 app.MapRazorPages();
 
 app.Run();
