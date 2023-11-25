9012e43dbae760b9b8b9aefb99048cbdbbbe175b
TacoConKvass <e_frun@o2.pl>
2023-11-25 08:30:56 +0100

    [ Addition ] - Post quotes from the page!

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,10 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using ITClassProject.Pages.Shared;
+using ITClassProject.Database;
+using ITClassProject.Database.Models;
 
 namespace ITClassProject.Pages
 {
     public class IndexModel : _PageModel
     {
+		QuoteDbContext quoteDb = Database.Database.GetQuoteDbContext();
+		public void OnPost() {
+			string source = Request.Form["source"].ToString();
+			string quote = Request.Form["quote"].ToString();
+
+			Quote submittedQuote = new Quote();
+			submittedQuote.Author = TempData.Peek("username").ToString();
+			submittedQuote.Source = source;
+			submittedQuote.Content = quote;
+
+			quoteDb.AddQuote(submittedQuote);
+		}
     }
 }
