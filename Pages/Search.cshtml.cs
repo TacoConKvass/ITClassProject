8d366589faa5f3eb03a5d261356bb895c13e49a8
TacoConKvass <e_frun@o2.pl>
2023-11-27 07:57:29 +0100

    [ WIP ]

--- Pages/Search.cshtml.cs
+++ Pages/Search.cshtml.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ITClassProject.Pages.Shared;
+using ITClassProject.Database.Models;
 
 namespace ITClassProject.Pages
 {
     public class SearchModel : _PageModel
     {
+		List<Quote> searchedQuotes = new List<Quote>();
+		public void OnGet() {
+
+		}
     }
 }
